9153c98e78c0c55891451c86d6e6068df91cf3d1
Maicol Torres <torresmail1998@gmail.com>
2023-12-04 20:50:02 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/*===================================== toggle icon navbar ===========================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navabar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[herf*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+};
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duratio: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*===================================== typed js ===========================*/
+const typed = new Typed('.multiple-text',{
+    strings: ['Trading', 'YouTuber', 'Gamers'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
